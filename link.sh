260658f3745f62274025b9b64e4be0acac00ed70
Ilya Lisin <blacklacost@gmail.com>
2019-11-06 22:41:09 +0300

    upd

---
+++ link.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+keybindings_file="$HOME/.config/Code/User/keybindings.json"
+settings_file="$HOME/.config/Code/User/settings.json"
+
+
+if [ -f $keybindings_file ]; then
+    mv $keybindings_file $keybindings_file.backup
+fi
+if [ -h $keybindings_file ]; then
+    rm $keybindings_file
+fi
+ln vscode_keybindings.json $keybindings_file
+
+if [ -f $settings_file ]; then
+    mv $settings_file $settings_file.backup
+fi
+if [ -h $settings_file ]; then
+    rm $settings_file
+fi
+ln vscode_settings.json $settings_file
