6046e2196479dc35d2324c5fb43eef0bb6a4e0e5
Ilya Lisin <blacklacost@gmail.com>
2020-08-30 08:40:06 +0300

    upd

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // Скобки в стрелочной функции даже, когда нет аргументов
+  arrowParens: 'always',
+  // Удалять точку с запятой после инструкции
+  semi: false,
+  singleQuote: true,
+  // Запятая в конце массивов и объектов
+  trailingComma: 'all',
+}
