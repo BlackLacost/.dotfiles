43dbffcb17736394b744607fef36a13aa4989c12
Ilya Lisin <blacklacost@gmail.com>
2021-10-23 10:59:40 +0300

    fix subtitle copy script

--- config/mpv/scripts/copySub.js
+++ config/mpv/scripts/copySub.js
@@ -1,11 +1,11 @@
 function setClipboard(text) {
-  mp.commandv('run', 'powershell', 'set-clipboard "' + text + '"');
+  mp.commandv('run', 'powershell', 'set-clipboard "' + text + '"')
 }
 
 function copySub() {
-  var sub_text = mp.get_property('sub-text');
-  setClipboard(sub_text);
-  mp.osd_message('Copied to Clipboard: ' + sub_text);
+  var sub_text = mp.get_property('sub-text').replace(/\n/g, ' ')
+  setClipboard(sub_text)
+  mp.osd_message('Copied to Clipboard: ' + sub_text)
 }
 
-mp.add_key_binding('Ctrl+x', 'copySub', copySub);
+mp.add_key_binding('', 'copySub', copySub)
