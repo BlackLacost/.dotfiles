dbf3824dc06b03bb71aea4d92370801f7450175d
Ilya Lisin <blacklacost@gmail.com>
2021-12-06 16:30:18 +0300

    nvim auto close tags

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -20,4 +20,6 @@ return require('packer').startup(function()
     'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons' }
   }
+
+  use { 'windwp/nvim-ts-autotag' }
 end)
--- config/nvim/lua/treesitter-config/init.lua
+++ config/nvim/lua/treesitter-config/init.lua
@@ -1,3 +1,15 @@
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
+    underline = true,
+    virtual_text = {
+      spacing = 5,
+      severity_limit = 'Warning',
+    },
+    update_in_insert = true,
+  }
+)
+
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {"html", "javascript", "lua", "typescript"},
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
@@ -5,5 +17,8 @@ require'nvim-treesitter.configs'.setup {
     enable = true,              -- false will disable the whole extension
     additional_vim_regex_highlighting = false,
   },
+  autotag = {
+    enable = true
+  }
 }
 
