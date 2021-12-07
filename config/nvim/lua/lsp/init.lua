32955c92a9651ad111a985f03bd7fb0fe7c1a649
Ilya Lisin <blacklacost@gmail.com>
2021-12-07 17:09:14 +0300

    global use & vim for lua lsp

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -10,13 +10,17 @@ local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.on_server_ready(function(server)
     local opts = {}
 
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
+    if server.name == 'sumneko_lua' then
+      opts.settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use'}
+          }
+        }
+      }
+    end
 
     -- This setup() function is exactly the same as lspconfig's setup function.
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     server:setup(opts)
 end)
-
