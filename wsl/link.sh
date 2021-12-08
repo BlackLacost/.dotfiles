2b4dfc690bca7d09e73becae77c970ec9f5ec755
Ilya Lisin <blacklacost@gmail.com>
2021-12-08 13:41:21 +0300

    link .gitconfig for wsl

--- wsl/link.sh
+++ wsl/link.sh
@@ -5,5 +5,7 @@ ln -sr $HOME/.dotfiles/config/vifm ~/.config -f
 ln -sr $HOME/.dotfiles/config/nvim ~/.config -f
 
 # Link files
-ln -s $HOME/.dotfiles/wsl/.bashrc ~ -f
+ln -s $HOME/.dotfiles/config/git/.gitconfig ~ -f
 ln -s $HOME/.dotfiles/config/npm/.npmrc ~ -f
+ln -s $HOME/.dotfiles/wsl/.bashrc ~ -f
+
