b9dcb4a08ef9e63d9d280da735f9a0c905f90e49
Ilya Lisin <blacklacost@gmail.com>
2025-03-12 14:12:29 +0300

    remove deprecated files

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Скобки в стрелочной функции даже, когда нет аргументов
-  arrowParens: 'always',
-  // Удалять точку с запятой после инструкции
-  semi: false,
-  singleQuote: true,
-  // Запятая в конце массивов и объектов
-  trailingComma: 'all',
-}
