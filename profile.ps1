c313fc1fd449a60204827b8b8aad848b69e3f324
Ilya Lisin <blacklacost@gmail.com>
2025-03-14 07:05:48 +0300

    delete deprecated code

--- profile.ps1
+++ profile.ps1
@@ -6,18 +6,22 @@ catch {
 
 Set-Alias -Name v -Value nvim
 Set-Alias -Name c -Value code
-Set-Alias -Name g -Value git
 
-function cddc() { Set-Location ~/Documents; }
-function cddl() { Set-Location d:/Downloads; }
-function cddt() { Set-Location ~/Desktop; }
+Set-Alias -Name g -Value git
+# TODO: Git aliases from https://github.com/gluons/powershell-git-aliases/blob/master/src/aliases.ps1
+function gsw {
+  git switch $args
+}
+function gswc {
+  git switch --create $args
+}
+function gbc {
+  git branch --show-current
+}
+
 function cdd() { Set-Location ~/.dotfiles; }
 function cdh() { Set-Location ~; }
 function cdc() { Set-Location d:/code; }
-function cdw() { Set-Location d:/code/constructor-player }
-function cds() { Set-Location c:/Users/black/scoop/apps; }
-
-function jnote() { Set-Location ~/python; jupyter notebook; }
 function cr() { code -r . | Invoke-Expression; }
 function nrs() { npm run start | Invoke-Expression; }
 function nrd() { npm run dev | Invoke-Expression; }
@@ -26,84 +30,6 @@ function nrt() { npm run test | Invoke-Expression; }
 function ghc() {
   gh repo list --limit 1000 | fzf | foreach { "gh repo clone {0}" -f ($_ -split '\t') } | Invoke-Expression
 }
-
-function gg() {
-  git hist
-}
-
-function gga() {
-  git hista
-}
-
-function gpush() { git push; }
-function gpushf() { git push -f; }
-function gpull() { git pull; }
-function gfetch() { git fetch; }
-function gst() { git status; }
-function ga($file) {
-  if ($file) {
-    git add $file;
-  }
-  else {
-    git add .;
-  }
-}
-function gcmm($commit) {
-  if ($commit) {
-    git commit -m $commit;
-  }
-  else {
-    git commit;
-  }
-}
-function gcma($commit) {
-  if ($commit) {
-    git commit --amend -m $commit;
-  }
-  else {
-    git commit --amend --no-edit;
-  }
-}
-function gacmm($commit) { git add .; gcmm($commit); }
-function gacma($commit) { git add .; gcma($commit); }
-function gdiff($file) { git diff HEAD $file; }
-function gdiffc($file) { git diff HEAD $file --cached; }
-
-function Compare-GitFiles {
-  param (
-    $File
-  )
-
-  if ($File) {
-    git diff HEAD $File
-  }
-  else {
-    git diff HEAD
-  }
-}
-Set-Alias -Name ggd -Value Compare-GitFiles
-
-function Get-GitLog {
-  param (
-    [bool]$All = $false
-  )
-
-  if ($All) {
-    git log `
-      --graph `
-      --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' `
-      --abbrev-commit `
-      --all
-  }
-  else {
-    git log `
-      --graph `
-      --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' `
-      --abbrev-commit `
-
-  }
-}
-Set-Alias -Name glg -Value Get-GitLog
 
 function Find-TaskPidByPort {
   param (
@@ -130,32 +56,8 @@ function Reload-Powershell {
   exit
 }
 
-# Windows-OSX-Linux consistency
-function rmd($dst) {
-  Remove-Item $dst -Recurse -Force
-}
-
-# Windows-OSX-Linux consistency
-function ll($dst) {
-  Get-ChildItem $dst
-}
-
-function Set-Proxy {
-  param (
-    $ProxyScriptUrl = 'https://antizapret.prostovpn.org/proxy.pac'
-  )
-  Set-ItemProperty `
-    -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' `
-    -Name 'AutoConfigURL' `
-    -Value $ProxyScriptUrl
-  Write-Output "Установлен proxy script $ProxyScriptUrl"
-}
-
-function Get-Proxy {
-  Get-ItemProperty `
-    -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' `
-    -Name 'AutoConfigURL' `
-  | Select-Object "AutoConfigURL"
+function rmd() {
+  Remove-Item -Recurse -Force $args;
 }
 
 function OnViModeChange {
