875d4a65454be4078b5ca9b2831d80943b7c87c3
Ilya Lisin <blacklacost@gmail.com>
2025-05-14 16:01:27 +0300

    upd

--- win-init.ps1
+++ win-init.ps1
@@ -1,9 +1,8 @@
 $isAdmin = [bool]([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 if ($isAdmin) {
-  Write-Host "Скрипт запущен от имени администратора."
   winget install --silent --accept-package-agreements --accept-source-agreements github.cli git.git microsoft.powershell
   Set-ExecutionPolicy Unrestricted -Scope CurrentUser
 }
 else {
-  Write-Error "Запустите скрипт под администратором"
+  Write-Error "Run this file as Admin"
 }
